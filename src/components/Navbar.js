c0c32d8ffcfca2da9982e82ac88ffa59755a4085
Landon Waddell <Landon.waddell@gmail.com>
2021-09-19 22:50:01 -0600

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,10 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-import { Container, Row, Col } from "react-bootstrap";
+import {  Row, Col } from "react-bootstrap";
 import { makeStyles } from '@material-ui/core/styles';
 import MobileRightMenuSlider from '@material-ui/core/Drawer'
 import {
     AiFillGithub,
-    AiOutlineTwitter,
-    AiFillInstagram,
   } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { GoBook } from "react-icons/go";
